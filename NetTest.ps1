b315ff454df9476636cedc8e06b9f7f6935eec08
asklootz <askl@uia.no>
2025-01-17 16:59:47 +0100

    Uploading NetTest script

---
+++ NetTest.ps1
@@ -0,0 +1,15 @@
+﻿while($true)
+{
+    if (Test-Connection www.google.com -Quiet)
+    {
+        ECHO "Internet is back"
+        $voice = New-Object -ComObject Sapi.spvoice
+        $voice.rate = 0
+        $voice.speak("Internet is back!")
+        break
+    }
+    else
+    {
+        ECHO "NO INTERNET"
+    }
+}
